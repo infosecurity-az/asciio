1615540434bd2c0a761b752dd065aac59fe632e9
infosecurity-az <emincik_azey@mail.ru>
2013-08-13 16:11:26 +0500

    Update skript.sh

--- skript.sh
+++ skript.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+#emighuliev
+
 
 for ((i=1; i<=10000; i++)); 
 do
     var=$(find ./ -name 'meta.json')
-    if [[ -n $var ]]
+    if [[ -n $var ]] #message length true ? 
     then
-    if [ -f $var ]
+    if [ -f $var ] #true file exists ?
     then
-    cp $var /home/test/fuckascii/std$i
+    cp $var /home/test/fuckascii/std$i #copy file :@@@
     exit
     fi
     fi
